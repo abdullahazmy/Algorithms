f9f2eaeb8f2e19b58f97feba0fc39a6f9ab2f3e9
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-06-26 06:53:02 +0300

    Update insertion_sort.cpp

--- sorting-algo/insertion_sort.cpp
+++ sorting-algo/insertion_sort.cpp
@@ -1,17 +1,16 @@
 void insertion_sort(vector<int> &nums)
 {
-    for (int i = 1; i < (int)nums.size(); i++)
+    int n = nums.size();
+    for (int i = 1; i < n; i++)  // i starts from 1 and goes up to n-1
     {
         int key = nums[i];
         int j = i - 1;
-        while (nums[j] > key && j >= 0)
+        while (j >= 0 && nums[j] > key)  // Corrected condition: j >= 0 should be checked first
         {
-            // Right Shifting for elements that are more than nums[i]
+            // Right Shift for nums[i]
             nums[j + 1] = nums[j];
             j--;
         }
-        nums[j + 1] = key; // Now return the overridn elemnt to it's position
+        nums[j + 1] = key;  // Insert key at the correct position
     }
 }
-
-// Takes complexity of O(n^2)
